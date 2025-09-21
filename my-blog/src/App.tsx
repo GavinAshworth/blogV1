86708bdae698e1b3f682b5607927baa60ab3d2d0
GavinAshworth <gavinashworth315@gmail.com>
2025-09-20 22:05:10 -0400

    hashrouter

--- my-blog/src/App.tsx
+++ my-blog/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import WeekPage from './pages/content.js';
 
 function App() {
